0b06e4a05ba4bf318ab736d54f30ffa0aa45415c
Daniel Goycochea <danielgoycocheardz@gmail.com>
2024-01-21 13:24:21 -0600

    feat: add header

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import styles from './header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles['header__navbar']}>
+        <Image
+          width={96}
+          height={41}
+          src="/img/logo-sochi-color.png"
+          alt="Logo"
+        />
+        <Image
+          width={96}
+          height={41}
+          src="/img/logo-claro-sports.png"
+          alt="Logo"
+        />
+      </nav>
+    </header>
+  )
+}
