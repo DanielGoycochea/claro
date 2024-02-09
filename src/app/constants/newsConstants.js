2c9a87c61a20f6173d8c6bff3d4ed23baae89927
Daniel Goycochea <43871466+DanielGoycochea@users.noreply.github.com>
2024-02-09 16:59:23 -0600

    Update newsConstants.js

--- src/app/constants/newsConstants.js
+++ src/app/constants/newsConstants.js
@@ -3,24 +3,24 @@ export const news = [
     id: 1,
     src: '/img/team-latam.png',
     title: 'TEAM LATAM',
-    url: 'https://www.youtube.com/embed/5vKC6Sci16g?si=xeKAHbyTA_a-VFh2',
+    url: 'https://www.youtube.com/embed/5vKC6Sci16g?autoplay=1&si=xeKAHbyTA_a-VFh2',
   },
   {
     id: 2,
     src: '/img/travel-meets-fashion.png',
     title: 'TRAVEL MEETS FASHION',
-    url: 'https://www.youtube.com/embed/mZela1mQMwU?si=uSzPHwPcGT7ee_At',
+    url: 'https://www.youtube.com/embed/mZela1mQMwU?autoplay=1&si=uSzPHwPcGT7ee_At',
   },
   {
     id: 3,
     src: '/img/velocidad-riesgo.png',
     title: 'VELOCIDAD Y RIESGO',
-    url: 'https://www.youtube.com/embed/vQ6VNayEE54?si=qBk2RSxxe4TSEs0t',
+    url: 'https://www.youtube.com/embed/vQ6VNayEE54?autoplay=1&si=qBk2RSxxe4TSEs0t',
   },
   {
     id: 4,
     src: '/img/experiencia.png',
     title: 'EXPERIENCIA MULTIMEDIA',
-    url: 'https://www.youtube.com/embed/GR_FNLXzNlM?si=WHLOlV2tABh9N0rl',
+    url: 'https://www.youtube.com/embed/GR_FNLXzNlM?autoplay=1&si=WHLOlV2tABh9N0rl',
   },
 ]
